919848a4c4afbd74fb5800b3e24ec1fe5b2b05e5
Muride <45217103+Muride@users.noreply.github.com>
2024-06-05 01:58:35 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ $(function(){
 		$(".menu-contents").toggleClass("fadedisplay");
 	});
 });
+
+$(function(){
+	$(".work").on('mouseover', function(){
+		$(this).find(".work-name").addClass("inv");
+		$(this).find(".released").addClass("released-inv");
+		$(this).find(".study").addClass("study-inv");
+		$(this).find(".purple").addClass("exp");
+	});
+	$(".work").on('mouseout', function(){
+		$(this).find(".work-name").removeClass("inv");
+		$(this).find(".released").removeClass("released-inv");
+		$(this).find(".study").removeClass("study-inv");
+		$(this).find(".purple").removeClass("exp");
+	});
+});
